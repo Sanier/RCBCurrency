4692041e65ff4c8cd86eecedfc5ba50b4b297b8f
Sanier <sanier20@mail.ru>
2024-02-23 11:47:22 +0300

    Add tests

---
+++ RCBCurrency.Tests/Controllers/CurrenciesDataControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RCBCurrency.Controllers;
+using RCBCurrency.Services.Implementation;
+using RCBCurrency.Services.Interfaces;
+
+namespace RCBCurrency.Tests.Controllers
+{
+    [TestFixture]
+    public class CurrenciesDataControllerTest
+    {
+        private ICurrencyService _currencyService;
+
+        private CurrenciesDataController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _currencyService = new CurrencyService();
+            _controller = new CurrenciesDataController(_currencyService);
+        }
+
+        [Test]
+        public async Task Currencies_ReturnsView_WhenStatusCodeIsSuccess()
+        {
+            var result = await _controller.Currencies() as ViewResult;
+
+            Assert.IsNotNull(result?.Model);
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+    }
+}
---
+++ RCBCurrency.Tests/Services/CurrencyServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using RCBCurrency.Domain.Enums;
+using RCBCurrency.Services.Implementation;
+using RCBCurrency.Services.Interfaces;
+
+namespace RCBCurrency.Tests.Services
+{
+    [TestFixture]
+    public class CurrencyServiceTest
+    {
+        private ICurrencyService _currencyService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _currencyService = new CurrencyService();
+        }
+
+        [Test]
+        public async Task GetCurrenciesData_ReturnsSuccessStatusCode_WhenCalledWithValidData()
+        {
+            // Arrange
+            var expectedStatusCode = StatusCode.Success;
+
+            // Act
+            var result = await _currencyService.GetCurrenciesData();
+
+            // Assert
+            Assert.That(result.StatusCode, Is.EqualTo(expectedStatusCode));
+        }
+    }
+}
